de8a478ab97b4dbe12f4449b71a7219c91a04d9c
joshuamiller-create <joshua.miller@tcc.sa.edu.au>
2026-03-30 15:51:55 +1030

    add-setup-script

---
+++ .local/share/bin/arch-config-script.sh
@@ -0,0 +1,30 @@
+#/bin/bash
+# Most essential pacman applications
+sudo pacman -S --noconfirm dnsutils keyd freerdp remmina imv
+
+# Active Directory
+sudo pacman -S --noconfirm samba smbclient
+sudo pacman -S --noconfirm bind
+
+# sudo cp <path> /etc/systemd/resolved.conf
+sudo systemctl restart systemd-resolved
+
+# Set up KeyD
+sudo cp etc/keyd.conf /etc/keyd/keyd.conf
+
+sudo pacman -S --noconfirm gimp caligula
+
+
+# Most essential AUR applications
+yay -Sua brave-bin ente-auth-bin
+# Less essential AUR applications
+yay -Sua ascii-image-converter beeper-v4-bin freetube-bin librewolf-bin
+
+
+# # Misc
+sudo pacman -S --noconfirm gamemode
+sudo pacman -S --noconfirm flatpak
+sudo pacman -Syu
+
+# # Cleanup phase
+sudo pacman -Rnu chromium
